1c47778dcd8fb72768aa18b76c8ed394e30808bc
devanshTandon00 <dtdansh@gmail.com>
2020-09-05 19:37:01 +0530

    Delete User/Task

--- src/index.js
+++ src/index.js
@@ -71,7 +71,6 @@ app.patch('/users/:id', async (req, res) => {
         })
     }
 
-
     // check if there is no user since mongoDB is gonna consider it as a sucess so we need to handle that
     try{
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
@@ -128,13 +127,57 @@ app.get('/tasks/:id', async (req, res) => {
  */
 
 app.patch('/tasks/:id', async (req, res) => {
-    try{
-        const task = Task.findByIdAndUpdate(req)
-    } catch(e){
+    // Object.keys returns array of given objects
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValid = updates.every((update) => {
+        allowedUpdates.includes(updates)
+    })
 
+    if(!isValid){
+        return res.status(400).send({
+            "error": "Invalid updates!"
+        })
+    }
+
+    try{
+        // returns the new modified object and runsValidation according to the model's schema
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!task)
+            return res.status(404).send()
+        
+        res.send(task)
+    } catch(e){
+        res.status(400).send(e)   
     }
 })
 
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user)
+            return res.status(404).send()
+
+        res.send(user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task)
+            return res.status(404).send()
+            
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up and running on port ' + port)
