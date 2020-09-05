80afa1b203785b86d38d76eb2bdb2bfee0913c7d
devanshTandon00 <dtdansh@gmail.com>
2020-09-05 21:41:24 +0530

    CommitBeforeClose

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/users')
-const Task = require('./models/task')
-const userRouter = require('./routers/userRouter')
-const taskRouter = require('./routers/taskRouter')
-const app = express()
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/userRouter");
+const taskRouter = require("./routers/taskRouter");
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // returns middleware that only parses json
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up and running on port ' + port)
-})
-
+  console.log("Server is up and running on port " + port);
+});
