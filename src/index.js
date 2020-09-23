f6542e77284e267e54d5e644482a915beed4b5c7
devanshTandon00 <dtdansh@gmail.com>
2020-09-23 17:29:19 +0530

    Working on user secuirity

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ const port = process.env.PORT || 3000;
 
 // returns middleware that only parses json
 app.use(express.json());
+
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up and running on port " + port);
 });
+
+const bycrpt = require("bycrptjs");
+
+const myFunction = async () => {
+  const pass = "Red@123";
+  const hashed = await bycrpt.hashed(pass, 8);
+
+  const isMatch = await bycrpt.compare(pass, hashed);
+};
+
+myFunction();
--- src/models/task.js
+++ src/models/task.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // created a task model
-const Task = mongoose.model('task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean, 
-        default: false
-    }
-})
+const Task = mongoose.model("test", {
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-module.exports = Task
+module.exports = Task;
