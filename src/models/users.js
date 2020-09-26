a9b153d532aba8c2d7aefd7448de9e9b6e11944a
devanshTandon00 <dtdansh@gmail.com>
2020-09-26 20:15:33 +0530

    Implemented Login

--- src/models/users.js
+++ src/models/users.js
@@ -16,7 +16,9 @@ const userSchema = new mongoose.Schema({
     trim: true,
     lowercase: true,
     validate(value) {
-      if (!validator.isEmail(value)) throw new Error("Invalid email");
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
     },
   },
   password: {
@@ -26,7 +28,7 @@ const userSchema = new mongoose.Schema({
     trim: true,
     validate(value) {
       if (value.toLowerCase().includes("password")) {
-        throw new Error('Cannot contain "password" ');
+        throw new Error('Password cannot contain "password"');
       }
     },
   },
@@ -34,7 +36,9 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
     validate(value) {
-      if (value < 0) throw new Error("Age must be positive");
+      if (value < 0) {
+        throw new Error("Age must be a postive number");
+      }
     },
   },
   tokens: [
@@ -47,48 +51,47 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-// use with class instance, for example User user, so call method on user
 userSchema.methods.getAuthToken = async function () {
   const user = this;
+
+  //generate token
   const token = jwt.sign({ _id: user._id.toString() }, "keepLearning");
 
-  // user.token = user.token.concat({ token: token });
-  // await user.save();
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
   return token;
+
+  //verify token
 };
 
-// use with class, for example User.findByCredentials
-//create a method to facilitate secure login
-userSchema.statics.findbyCredentials = async (email, password) => {
-  const user = await User.findOne({ email: email });
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
 
   if (!user) {
     throw new Error("Unable to login");
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) throw new Error("Unable to login");
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
   return user;
 };
 
-// we create a seperate schema to access middleware functions such as pre post for accessing state before/after
-// we cannot use arrow function in here, since we need to make use of 'this' keyword
-// the 'this' keyword will help us access the individual user
-// hash the plain text password before saving
+// Hash the plain text password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
 
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
   next();
 });
 
-// created a user model
-// when we pass in a object in the model as a second arg, mongoose converts it to a schema in the backgroun
+const User = mongoose.model("User", userSchema);
 
-const user = mongoose.model("user", userSchema);
-
-module.exports = user;
+module.exports = User;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
 const User = require("../models/users");
+const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -27,9 +27,6 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-// two option of handling find
-// 1. use promise .then() .catch() structure
-// 2. use callback functions
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -37,77 +34,62 @@ router.get("/users", async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
-
-  // User.find({}).then((users) => {
-  //     res.send(users)
-  // }).catch((error) => {
-  //     res.status(500).send()
-  // })
-
-  // User.find({}, (result, error) => {
-  //     if(error)
-  //        res.send(users)
-  //     else
-  //         console.log(result)
-  // })
 });
 
-// we use if condition since mongoDB returns an empty query result as a sucess as well
 router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).send();
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
     res.send(user);
   } catch (e) {
-    res.status(500).send;
+    res.status(500).send();
   }
 });
 
 router.patch("/users/:id", async (req, res) => {
-  // returns an array of elements of req.body
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
-  const isValid = updates.every((update) => allowedUpdates.includes(update));
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-  if (!isValid) {
-    return res.status(400).send({
-      error: "Invalid Updates!",
-    });
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
   }
 
-  // check if there is no user since mongoDB is gonna consider it as a sucess so we need to handle that
   try {
-    // Although this implementation is not wrong, it skips over the middleware we need to activate to
-    // hash the user passwords.
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-
     const user = await User.findById(req.params.id);
-    updates.forEach((update) => {
-      user[update] = req.body[update];
-    });
 
+    updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
 
     if (!user) {
       return res.status(404).send();
     }
 
-    // update old user and send modified user
     res.send(user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/users/:id", async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const user = await User.findByIdAndDelete(req.params.id);
 
-    if (!task) return res.status(404).send();
+    if (!user) {
+      return res.status(404).send();
+    }
 
-    res.send(task);
+    res.send(user);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(500).send();
   }
 });
 
