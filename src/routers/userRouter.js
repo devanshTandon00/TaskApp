0c94e82c19c3519ca2b2b9875de0a8558011710e
devanshTandon00 <dtdansh@gmail.com>
2020-10-04 17:20:05 +0530

    Commit for 10/4/20

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -29,7 +29,7 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users", auth, async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
